4532414d025e2d8de480cff68dd374d0e39e9c95
Austin Huizinga <austinth127@gmail.com>
2022-09-06 18:11:54 -0500

    minor menu styling and css condensing

--- frontend/components/graph/Menu.js
+++ frontend/components/graph/Menu.js
@@ -6,12 +6,18 @@ export const Menu = () => {
     if (show) {
         return (
             <ul
-                className="absolute flex flex-col"
+                className="absolute flex flex-col bg-slate-900 bg-opacity-80 gap-2 rounded-lg p-4"
                 style={{ top: anchorPoint.y, left: anchorPoint.x }}
             >
-                <button onClick="">Show Neighbors</button>
-                <button onClick="">Highlight Node</button>
-                <button onClick="">Track Node</button>
+                <button onClick="" className="gradient">
+                    Show Neighbors
+                </button>
+                <button onClick="" className="gradient">
+                    Highlight Node
+                </button>
+                <button onClick="" className="gradient">
+                    Track Node
+                </button>
             </ul>
         );
     }
