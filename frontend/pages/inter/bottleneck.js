9edd7024869896b121f263fc37674c9364a700a5
garito456 <garrett_parker1@baylor.edu>
2022-09-27 23:13:10 -0500

    Knot and Bottleneck JSON

--- frontend/pages/inter/bottleneck.js
+++ frontend/pages/inter/bottleneck.js
@@ -1,7 +1,16 @@
 import React from "react";
+import bottleneckData from "../../utils/antipatterns/bottleneck_service.json";
+import InterNodeVisLayout from "../../components/antipatterns/InterNodeVisLayout";
 
 const Bottleneck = () => {
-    return <div>Bottleneck</div>;
+    return (
+        <div>
+            <InterNodeVisLayout
+                //graphColorFn={getColor}
+                antipatternJSON={bottleneckData}
+            ></InterNodeVisLayout>
+        </div>
+    );
 };
 
 export default Bottleneck;
--- frontend/pages/inter/knot.js
+++ frontend/pages/inter/knot.js
@@ -1,7 +1,35 @@
 import React from "react";
+import theKnotData from "../../utils/antipatterns/the_knot.json";
+import InterNodeVisLayout from "../../components/antipatterns/InterNodeVisLayout";
 
 const Knot = () => {
-    return <div>Knot</div>;
+    // function getColor(node, threshold) {
+    //     let color = null;
+    //     if (!sccs) {
+    //         sccs = getSccs();
+    //     }
+    //     sccs.forEach((scc, index) => {
+    //         if (scc.includes(node.id)) {
+    //             if (scc.length >= threshold) {
+    //                 color = `rgb(255,102,102)`;
+    //             } else if (scc.length >= threshold / 2) {
+    //                 color = `rgb(102,102,255)`;
+    //             }
+    //         }
+    //         if (color) return;
+    //     });
+    //
+    //     return color ?? `rgb(100,100,100)`;
+    // }
+
+    return (
+        <div>
+            <InterNodeVisLayout
+                //graphColorFn={getColor}
+                antipatternJSON={theKnotData}
+            ></InterNodeVisLayout>
+        </div>
+    );
 };
 
 export default Knot;
