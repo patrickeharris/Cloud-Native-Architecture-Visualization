005fd68cd45289bcfae6c19f6f6a15ab4c67ca4b
garito456 <garrett_parker1@baylor.edu>
2022-10-19 16:30:40 -0500

    Duplicated service

--- frontend/pages/inter/duplicated-service.js
+++ frontend/pages/inter/duplicated-service.js
@@ -1,7 +1,7 @@
 import React from "react";
 import duplicatedServiceData from "../../utils/antipatterns/duplicated_service.json";
 import InterNodeVisLayout from "../../components/antipatterns/InterNodeVisLayout";
-import { getNeighbors, getDuplicates } from "../../utils/visualizer/nodeFunctions";
+import { getDuplicates } from "../../utils/visualizer/nodeFunctions";
 import { useAtom } from "jotai";
 import { graphDataAtom } from "../../utils/atoms";
 
@@ -10,16 +10,8 @@ const DuplicatedService = () => {
 
     function getColor(node, threshold) {
         let { nodes, links } = graphData;
-        let { duplicates } = getDuplicates(node, nodes);
-        //let numNeighbors = getNeighbors(node, links).nodes.length;
-        /*
-        if (numNeighbors > threshold) {
-            return `rgb(255,0,0)`;
-        }
-        if (numNeighbors > threshold / 2) {
-            return `rgb(255,160,0)`;
-        }
-        */
+        var duplicates = getDuplicates(node, nodes);
+        
         if(duplicates > 1){
             return `rgb(255,0,0)`;
         }
